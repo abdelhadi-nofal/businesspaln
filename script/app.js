b06b6b888825c7e4e74f9eb851f3c1a70f4ba741
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-02-24 16:33:44 +0200

    dsf

--- script/app.js
+++ script/app.js
@@ -6,29 +6,35 @@ var left=0
 function myfun (userAge) {
     var left=15-parseInt(userAge);
     document.getElementById("play").innerHTML = "You Can not play Right Now You Need More  " +left+ "  years";
-    
 }
-
-
 
 myfun(userAge)
 
 if (userAge>age){
     console.log("YOU CAN PLAY");
     document.getElementById("play").innerHTML = "You Can play ";
+    // var userInput=prompt("How Many Balls Do You Want? ");
+    //  document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Basketball.jpeg" alt="balls">');
+
 }else {
     alert("OPPS! SORRY YOUR AGE DOES NOT QUALIFED YOU TO PLAY THE GAME");
     document.write('<img src="https://iapdworld.org/wp-content/uploads/2019/08/Children-6-10-years-of-age.png" alt="childPic">');
 }
 
+var num=prompt("How Many Balls Do You Want To Play With? ");
+for (i = 0; i < num; i++) {
+    document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Basketball.jpeg" alt="balls">');
+  }
+
 function render(){
 var userName=prompt("Plz enter yr name");
 document.getElementById("name").innerHTML = "Your Name : "+userName;
 document.getElementById("age").innerHTML = "Your Age : "+userAge;
-
 }
 
 render();
+
+
 
 
 
