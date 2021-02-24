0f93689122b917ee63fc155ff82538161c667ce1
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-02-24 14:40:17 +0200

    addfun

--- script/app.js
+++ script/app.js
@@ -5,14 +5,23 @@ var age=14;
 
 var userAge=prompt("PLEASE ENTER YOUR AGE!! YOU HAVE TO BE +14 ");
 
+var left=0
 
+function myfun (userAge) {
+    var left=15-parseInt(userAge);
+    document.getElementById("play").innerHTML = "You Can not play Right Now You Need More  " +left+ "  years";
+}
+
+
+
+myfun(userAge)
 
 if (userAge>age){
     console.log("YOU CAN PLAY");
     document.getElementById("play").innerHTML = "You Can play ";
 }else {
     alert("OPPS! SORRY YOUR AGE DOES NOT QUALIFED YOU TO PLAY THE GAME");
-    document.getElementById("play").innerHTML = "You Can not play ";
+    // document.getElementById("play").innerHTML = "You Can not play Right Now You Need More  " +left+ "  years";
 }
 
 
@@ -21,6 +30,11 @@ var userName=prompt("Plz enter yr name");
 document.getElementById("name").innerHTML = "Your Name : "+userName;
 document.getElementById("age").innerHTML = "Your Age : "+userAge;
 
+// document.getElementById("").innerHTML="";
+{/* <h2 id=''></h2> */}
+// var ggg=document.getElementById('')
+
+// document
 
 
 
